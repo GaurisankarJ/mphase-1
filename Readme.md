efd2567fd2d1259f1663db89ac47d5943c40a0ba
Gaurisankar J <gaurisankarj1996@gmail.com>
2020-03-23 16:21:38 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Flask Server to Upload CSV files to Mongo Collections
+
+## Run [In mphase folder]
+
+```
+// To install virtual environment
+pip install virtualenv
+
+// To create virtual environment
+virtualenv venv
+
+// Activate virtual environment
+venv/bin/activate
+
+// Install dependencies
+pip3 install -r requirements.txt
+
+// Run server
+Python server.py
+
+// Run mongoDB
+mongod --dbpath ~/path/to/mphase/database
+```
---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import *
+import pymongo
+import pandas as pd
+
+app = Flask(__name__)  
+ 
+@app.route('/')  
+def home():  
+    return render_template("file_upload_form.html")  
+ 
+@app.route('/upload', methods = ['POST'])  
+def upload():  
+    if request.method == 'POST':  
+        f = request.files['file']
+        mongoClient = pymongo.MongoClient('localhost', 27017)
+        mongoDb = mongoClient['mphase']
+        collection = 'stock'
+        db = mongoDb[collection]
+        data = pd.read_csv(f)
+        data_json = json.loads(data.to_json(orient='records'))
+        names_key = {'% Deli. Qty to Traded Qty': '% Deli Qty to Traded Qty',
+                     'No. of Trades': 'No of Trades',
+                     'No.of Shares': 'No of Shares',
+                     'Total Turnover (Rs.)': 'Total Turnover (Rs)'
+                     }
+        for row in data_json:
+            for k, v in names_key.items():
+                for old_name in row:
+                     if k == old_name:
+                        row[v] = row.pop(old_name)
+        db.remove()
+        db.insert(data_json)
+        return render_template("success.html")
+  
+if __name__ == '__main__':  
+    app.run(debug=True) 
